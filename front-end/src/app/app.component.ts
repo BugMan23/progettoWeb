895dcec672435382de887ee3b114db55995bb863
sofift <sofiafoti2004@icloud.com>
2025-02-01 18:32:04 +0100

    push front end

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet } from '@angular/router';
+import {HomepageComponent} from './Components/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
+  imports: [
+    RouterOutlet
+  ],
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-end';
+  constructor(private router : Router) {
+    this.router.navigate(['/']);
+  }
 }
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent} from './Components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent }, // Homepage visibile all'avvio
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
